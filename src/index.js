1fb84199a3026efeacd8094d45975abf570c0d2e
shcher-olha <shcher.olha@gmail.com>
2023-11-27 16:37:11 +0100

    Inject the Java from Html

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ function refresWeather(response) {
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
-  
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
@@ -51,7 +50,37 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+function displayForecast() {
+  
+
+  let days = ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  let forecastHtml = "";
+
+days.forEach(function(day) {
+  forecastHtml = 
+  forecastHtml + 
+  `
+  <div class="weather-forecast-day">
+      <div class="weather-foreast-date">${day}</div>
+      <div class="weather-forecast-icon">🌤️</div>
+      <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max"> 18° </span>
+        <span class="weather-forecast-temperature-min"> 12° </span>
+      </div>
+    </div>
+  `;
+});
+
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+displayForecast();
+
+  
+
